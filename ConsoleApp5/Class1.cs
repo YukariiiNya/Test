b4846649e7ceb3078ec6f26e7dcddd07b87a59b8
YukariiiNya <fsfsfsdfdfssdf@gmail.com>
2024-11-29 18:17:42 +0900

    Add files via upload

---
+++ ConsoleApp5/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InspectorLib
+{
+    public class FunctionInsp
+    {
+        private string inspectionName = "Автоинспекция г. Чита";
+        private string chiefInspector = "Васильев Василий Иванович";
+        private List<string> workers = new List<string>
+        {
+            "Иванов И.И.", "Зиронов Т.А.", "Миронов А.В.", "Васильев В.И."
+        };
+
+        public string GetInspector()
+        {
+            return chiefInspector;
+        }
+
+        public string GetCarInspection()
+        {
+            return inspectionName;
+        }
+
+        public bool SetInspector(string fullname)
+        {
+            if (workers.Contains(fullname))
+            {
+                chiefInspector = fullname;
+                return true;
+            }
+            return false;
+        }
+
+        public string GenerateNumber(string number, string symbol, string code = "75")
+        {
+            return $"{symbol.ToUpper()}{number}_{code}";
+        }
+
+        public List<string> GetWorker()
+        {
+            return workers;
+        }
+
+        public void AddWorker(string fullname)
+        {
+            if (!workers.Contains(fullname))
+            {
+                workers.Add(fullname);
+            }
+        }
+    }
+}
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using InspectorLib;
+
+namespace IvanovTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var insp = new FunctionInsp();
+
+            Console.WriteLine("Тестирование библиотеки InspectorLib\n");
+
+            // 1. Получение главного инспектора
+            Console.WriteLine("Главный инспектор: " + insp.GetInspector());
+
+            // 2. Получение названия автоинспекции
+            Console.WriteLine("Название автоинспекции: " + insp.GetCarInspection());
+
+            // 3. Смена главного инспектора
+            Console.WriteLine("Попытка сменить главного инспектора на 'Миронов А.В.': " +
+                              (insp.SetInspector("Миронов А.В.") ? "Успех" : "Неудача"));
+            Console.WriteLine("Главный инспектор после смены: " + insp.GetInspector());
+
+            // 4. Генерация госномера
+            string generatedNumber = insp.GenerateNumber("123", "A", "75");
+            Console.WriteLine("Сгенерированный номер: " + generatedNumber);
+
+            // 5. Список сотрудников
+            Console.WriteLine("Список сотрудников:");
+            foreach (var worker in insp.GetWorker())
+            {
+                Console.WriteLine(worker);
+            }
+
+            // 6. Добавление нового сотрудника
+            insp.AddWorker("Петров П.П.");
+            Console.WriteLine("Список сотрудников после добавления:");
+            foreach (var worker in insp.GetWorker())
+            {
+                Console.WriteLine(worker);
+            }
+        }
+    }
+}
